9b5fcbe613f11a11b40241fd6a1ac33fa279bc3e
Sam Goldstein <sammy@gainfitness.com>
2014-11-23 12:17:55 -0800

    rails scaffolding

--- README.md
+++
@@ -1,3 +0,0 @@
-spykeball-api
-=============
-`rage`
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpykeballApi::Application.config.secret_token = '2a9430b9be59b328450fe824e03ae11d2b6be2c755a24c5c492481225605564eebc5d490fcadf7a12958a71a51257e1510788aecf5bceb88947b3f00b648d472'
