282f8982f2fa76562f3478767801081bf2272d8c
Sam Goldstein <sammy@gainfitness.com>
2015-09-13 17:49:12 -0400

    whatever gs

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Spikeball::Application.config.secret_token = '6e3b40233da166bbe8ded88c4a4f12845f389e1a90d5cd6b6a7af10441ec1e4e95c7fa83603b5f4a766be59e3128074ab19ecf2f1c42dfafd5abdbfdfa6b0b93'
